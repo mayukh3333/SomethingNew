67c16bdd16bd395a7afebf0fb75b77f09addbd53
Mayukh Dewan <mayukhdewan@gmail.com>
2020-09-07 11:53:47 +0530

    Init Commit

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+import flask
+from flask import Flask,render_template
 
+app = Flask(__name__)
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
